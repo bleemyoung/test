501bdada70e35cd0e457a9c06631d6714833c256
bleemyoung <670226461@qq.com>
2024-03-22 14:03:11 +0800

    字符串的replace功能

--- js/test.js
+++ js/test.js
@@ -1,6 +1,13 @@
-Array.prototype.duplicator = function () {
-    let s = this.concat(this)
-    return s
-}
-let t = [1, 2, 3, 4, 5].duplicator()
-console.log(t)
+// Array.prototype.duplicator = function () {
+//     let s = this.concat(this)
+//     return s
+// }
+// let t = [1, 2, 3, 4, 5].duplicator()
+// console.log(t)
+
+let str = "310110198211203711"
+let res = str.replace(
+    /(.{6}).*(.{4})/,
+    "$1********$2"
+)
+console.log(res);
