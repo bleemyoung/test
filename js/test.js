faa5fcc4c169598a9ca134c34b7047384cd569c3
bleemyoung <670226461@qq.com>
2024-03-29 10:21:27 +0800

    简单题

--- js/test.js
+++ js/test.js
@@ -5,9 +5,41 @@
 // let t = [1, 2, 3, 4, 5].duplicator()
 // console.log(t)
 
-let str = "310110198211203711"
-let res = str.replace(
-    /(.{6}).*(.{4})/,
-    "$1********$2"
-)
-console.log(res);
+// let str = "310110198211203711"
+// let res = str.replace(
+//     /(.{6}).*(.{4})/,
+//     "$1********$2"
+// )
+// console.log(res);
+
+/**
+ * @param {number} mainTank
+ * @param {number} additionalTank
+ * @return {number}
+ */
+var distanceTraveled = function (mainTank, additionalTank) {
+
+    if (mainTank < 5) {
+        return mainTank * 10
+    } else {
+        let res = 0
+        for (; mainTank >= 5 && additionalTank >= 1; mainTank -= 5) {
+            additionalTank--
+            mainTank++
+            res += 50
+        }
+        // while(mainTank >= 5){
+        //     mainTank -= 5
+        //     additionalTank--
+        //     mainTank++
+        //     res += 50
+        // }
+        // console.log(mainTank);
+        res += mainTank * 10
+        return res
+    }
+
+};
+console.log(distanceTraveled(5, 10));
+// https://leetcode.cn/problems/total-distance-traveled/description/
+
