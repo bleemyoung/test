e1e0b105b0420777ea3c0e79dec583abb70ce486
bleemyoung <670226461@qq.com>
2024-11-23 00:16:29 +0800

    1

--- js/test.js
+++ js/test.js
@@ -1,50 +1,26 @@
-// Array.prototype.duplicator = function () {
-//     let s = this.concat(this)
-//     return s
-// }
-// let t = [1, 2, 3, 4, 5].duplicator()
-// console.log(t)
-
-// let str = "310110198211203711"
-// let res = str.replace(
-//     /(.{6}).*(.{4})/,
-//     "$1********$2"
-// )
-// console.log(res);
-// var minOperations = function (nums) {
-//     let n = nums.length
-//     for (let i = 0; i < n - 2; i++) {
-//         if (nums[i] == 0) {
-//             for (let j = 0; j < 3; j++) {
-//                 nums[i+j]=nums[i+j]==1?0:1
-//             }
-//         }
-//     }
-//     if (nums[n - 2] == 0 || nums[n - 1 == 0]) return false
-//     return true
-// };
-// console.log(minOperations([[0, 1, 1, 1, 0, 0]]));
-var minChanges = function (n, k) {
+/**
+ * @param {number} n
+ * @param {string[]} commands
+ * @return {number}
+ */
+var finalPositionOfSnake = function (n, commands) {
     let ans = 0
-    if (n != (n | k)) {
-        return -1
-    }
-    let count1 = 0
-    while (n > 0) {
-        if (n % 2 === 1) {
-            count1 += 1
+    for (let command of commands) {
+        switch (command) {
+            case 'UP':
+                ans -= n
+                break;
+            case 'RIGHT':
+                ans++
+                break;
+            case 'DOWN':
+                ans += n
+                break;
+            case 'LEFT':
+                ans--
+                break;
         }
-        n = Math.floor(n / 2)
     }
-    let count2 = 0
-    while (k > 0) {
-        if (k % 2 === 1) {
-            count2 += 1
-        }
-        k = Math.floor(k / 2)
-    }
-    console.log(count1, count2);
-    return count1 - count2
+    return ans
 };
-console.log(minChanges(13, 4));
-
+console.log(finalPositionOfSnake(2, ["RIGHT", "DOWN"]));
--- js/力扣/3258.统计满足 K 约束的子字符串数量 I/run.js
+++ js/力扣/3258.统计满足 K 约束的子字符串数量 I/run.js
@@ -6,14 +6,28 @@
 var countKConstraintSubstrings = function (s, k) {
     let ans = 0
     const n = s.length
+    let mem = new Array(n).fill(0).map(() => new Array(n).fill(-10))
     function windowCount(len) {
-        if (len == 1) {
-            return n
-        }
+        // if (len == 1) {
+        //     return n
+        // }
         let left = 0
         let count = 0
         let ans = 0
         for (let right = 0; right < n; right++) {
+            if (right > 0 && mem[left][right - 1] >= 0) {
+
+                if (right - left + 1 == len) {
+                    count = mem[left][right - 1] + (s[right] == '1' ? 1 : 0)
+                    mem[left][right] = count
+                    if (count <= k || len - count <= k) {
+                        ans++
+                    }
+                    continue
+                }
+            } else {
+
+            }
             if (s[right] == '1') count++
             while (right - left + 1 > len) {
                 if (s[left] == '1') {
@@ -22,6 +36,7 @@ var countKConstraintSubstrings = function (s, k) {
                 left++
             }
             if (right - left + 1 == len) {
+                mem[left][right] = count
                 if (count <= k || len - count <= k) {
                     ans++
                 }
@@ -38,4 +53,4 @@ var countKConstraintSubstrings = function (s, k) {
 };
 
 
-console.log(countKConstraintSubstrings("10101", 1));
+console.log(countKConstraintSubstrings("10101", 1));//12
