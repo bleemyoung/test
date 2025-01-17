e7797d118b37ae8fb0692abc91cf0609b8202e2a
bleemyoung <670226461@qq.com>
2025-01-17 17:16:23 +0800

    1、3097. 或值至少为 K 的最短子数组 II_medium 2、保存进度

--- js/test.js
+++ js/test.js
@@ -8,3 +8,51 @@
 // };
 // // 3274. 检查棋盘方格颜色是否相同
 // console.log(checkTwoChessboards("a1", "c3"));//true
+var numRookCaptures = function (board) {
+    function findRook() {
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (board[i][j] === 'R') {
+                    return [i, j];
+                }
+            }
+        }
+    }
+    let rook = findRook();
+    let count = 0;
+    for (let i = rook[0] - 1; i >= 0; i--) {
+        if (board[i][rook[1]] === 'p') {
+            count++;
+            break;
+        } else if (board[i][rook[1]] === 'B') {
+            break;
+        }
+    }
+    for (let i = rook[0] + 1; i < 8; i++) {
+        if (board[i][rook[1]] === 'p') {
+            count++;
+            break;
+        } else if (board[i][rook[1]] === 'B') {
+            break;
+        }
+    }
+    for (let i = rook[1] - 1; i >= 0; i--) {
+        if (board[rook[0]][i] === 'p') {
+            count++;
+            break;
+        } else if (board[rook[0]][i] === 'B') {
+            break;
+        }
+    }
+    for (let i = rook[1] + 1; i < 8; i++) {
+        if (board[rook[0]][i] === 'p') {
+            count++;
+            break;
+        } else if (board[rook[0]][i] === 'B') {
+            break;
+        }
+    }
+    return count;
+};
+let info = [[".", ".", ".", ".", ".", ".", ".", "."], [".", ".", ".", "p", ".", ".", ".", "."], [".", ".", ".", "R", ".", ".", ".", "p"], [".", ".", ".", ".", ".", ".", ".", "."], [".", ".", ".", ".", ".", ".", ".", "."], [".", ".", ".", "p", ".", ".", ".", "."], [".", ".", ".", ".", ".", ".", ".", "."], [".", ".", ".", ".", ".", ".", ".", "."]]
+console.log(numRookCaptures(info));
