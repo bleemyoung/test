bf7fe34e21495f4e47410bc3dd3baca57ca5161c
bleemyoung <670226461@qq.com>
2026-06-18 22:52:53 +0800

    1

--- js/力扣/1344.时钟指针的夹角/run.js
+++ js/力扣/1344.时钟指针的夹角/run.js
@@ -3,14 +3,14 @@
  * @param {number} minutes
  * @return {number}
  */
-var angleClock = function(hour, minutes) {
+var angleClock = function (hour, minutes) {
     let one_min_angle = 6
-       let one_hour_angle = 30
+    let one_hour_angle = 30
     minutes_angle = one_min_angle * minutes
-        hour_angle = (hour % 12 + minutes / 60) * one_hour_angle
-    diff = Math.abs(minutes_angle-hour_angle)
-    return Math.min(diff,360-diff)
+    hour_angle = (hour % 12 + minutes / 60) * one_hour_angle
+    diff = Math.abs(minutes_angle - hour_angle)
+    return Math.min(diff, 360 - diff)
 };
 
 
-console.log(angleClock(12,30))
+console.log(angleClock(12, 30))
