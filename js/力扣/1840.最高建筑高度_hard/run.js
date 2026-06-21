3c21417d2ce5a44858c384a17cf3ccf80f97638d
bleemyoung <670226461@qq.com>
2026-06-21 09:51:55 +0800

    1840hard，后续总结

---
+++ js/力扣/1840.最高建筑高度_hard/run.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @param {number[][]} restrictions
+ * @return {number}
+ */
+var maxBuilding = function(n, restrictions) {
+    
+};
+
+// https://leetcode.cn/problems/maximum-building-height/solutions/3981306/liang-ci-sao-miao-chuan-bo-yue-shu-pytho-kigb/?envType=daily-question&envId=2026-06-20
